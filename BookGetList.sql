1771d01d525a172c84ed87d46303035e616f5f79
Mangesh Mangesh <79048195+mangeshkanawade@users.noreply.github.com>
2022-06-08 17:20:17 +0530

    Update BookGetList.sql
    Procedure

--- BookGetList.sql
+++ BookGetList.sql
@@ -1,31 +1,47 @@
-USE [LibraryManagement]
+USE [TRAINING]
 GO
-/****** Object:  StoredProcedure [dbo].[BooksGetList]    Script Date: 08-06-2022 17:17:13 ******/
+/****** Object:  StoredProcedure [dbo].[BooksGetList]    Script Date: 08-06-2022 17:19:49 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
+
+
 ALTER PROCEDURE [dbo].[BooksGetList]
-	@BookName VARCHAR(50) = null, @BookPublisherId INT = null, @BookCategoryId INT = null
+	@BookName VARCHAR(50) = null, @BookPublisherId INT = null, @BookCategoryId INT = null, @PageNumber INT = 1, @PageSize INT = 10
 	AS BEGIN
 
+	DECLARE @TotalRecords INT = 0
+
 	SELECT 
-		[Books].[BookId],							[Books].[BookName],
-		[BookCategories].[BookCategoryId],			[BookCategories].[BookCategoryName],
-		[BookPublishers].[BookPublisherId],			[BookPublishers].[BookPublisherName],	
-		[BookQuantity],								[Books].[IsActive],
-		[Books].[CreatedBy],						[Books].[CreatedOn],		
+		[Books].[BookId],
+		[Books].[BookName],
+		[BookCategories].[BookCategoryId],
+		[BookCategories].[BookCategoryName],
+		[BookPublishers].[BookPublisherId],	
+		[BookPublishers].[BookPublisherName],	
+		[BookQuantity],
+		[Books].[IsActive],
+		[Books].[CreatedBy],
+		[Books].[CreatedOn],
 		[Books].[ModifiedBy],
-		[Books].[ModifiedOn]						
+		[Books].[ModifiedOn], ROW_NUMBER() OVER (ORDER BY BookId) as RowNumber	into #TempBooks
 	FROM
 		[Books] Inner Join BookCategories On [BookCategories].BookCategoryId = [Books].BookCategoryId
 				Inner Join BookPublishers On [BookPublishers].BookPublisherId = [Books].BookPublisherId
 	Where
 		 ([Books].[BookName] LIKE '%'+@BookName+'%' OR @BookName IS NULL)
-		 AND  ([BookCategories].BookCategoryId = @BookCategoryId OR @BookCategoryId IS NULL) 	
-		 AND  ([BookPublishers].BookPublisherId  = @BookPublisherId OR @BookPublisherId IS NULL) 
-		 AND  Books.IsActive = 1 
-	ORDER BY 
-		 BookId 
+	AND  ([BookCategories].BookCategoryId = @BookCategoryId OR @BookCategoryId IS NULL) 	
+	AND  ([BookPublishers].BookPublisherId  = @BookPublisherId OR @BookPublisherId IS NULL)
 	
+	ORDER BY BookId 
+
+	SELECT @TotalRecords = COUNT(BookId) from #TempBooks
+	
+	
+	SELECT*,  @TotalRecords AS TotalRecords FROM #TempBooks
+	where RowNumber between (@PageNumber - 1) * @PageSize + 1 and @PageNumber * @PageSize
+
+	drop table #TempBooks
 END
